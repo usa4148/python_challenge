9ed0ea64b32adb743359e5a4d17f55d89cb2b881
Dan Cusick <dan.cusick@gmail.com>
2021-03-07 22:57:44 -0600

    Added some comments, deleted diagnostic prints commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,39 +3,50 @@
 # Data Scienct Boot Camp
 # python-challenge PyPoll
 #
-#
-#
+
 import os
 import csv
-import numpy
 
+# Open the text file for write
+pypoll = os.path.join('.','analysis', 'PyPoll.txt')
+pypollwriter = open(pypoll,'w')
+
+# Open the csv file with the votes
 csvpath = os.path.join('.', 'Resources', 'election_data.csv')
 
 with open(csvpath) as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter = ',')
     csv_header = next(csvreader)
-    #print(f"Header: {csv_header}")
 
-    #num_votes = sum(1 for row in csvfile)
-    #print(csvreader.line_num)
-    #print("num_votes", num_votes)
-
+    # Create a dict for the tabulation
     candidates = {}
 
     for vote in csvreader:
-        if  vote[2] in candidates:                #candidates.get(vote[2]) != None:
+        if  vote[2] in candidates:   # We already have the candidate in the dict, increment
             candidates[vote[2]] += 1
         else:
-            candidates[vote[2]] = 1
-            #print(f"{vote[2]}")
+            candidates[vote[2]] = 1  # We do not yet have the candidate in the dict - add him and 1 vote
+            
 
-
+# Assuming no undercount the total number of votes is the number of rows less the header
 total_v = csvreader.line_num - 1
+
+# Print to the console
 print("\nElection Results","\n-----------------------------")
 print("Total Votes:", (total_v),"\n-----------------------------")
-#print(" Total Votes:", num_votes,"\n-----------------------------")
-#print(" Winner: ", pnl_total,"\n-----------------------------")
-#print(f"{candidates}")
 for x in candidates:
     print(x,": ", "{:.3f}%".format(candidates[x] / total_v * 100), candidates[x])
+winner = max(candidates, key=candidates.get)
+print("-----------------------------")
+print("Winner: ", winner,"\n-----------------------------")
+
+# Print to the txt file
+print("\nElection Results","\n-----------------------------", file = pypollwriter)
+print("Total Votes:", (total_v),"\n-----------------------------", file = pypollwriter)
+for x in candidates:
+    print(x,": ", "{:.3f}%".format(candidates[x] / total_v * 100), candidates[x], file = pypollwriter)
+print("-----------------------------", file = pypollwriter)
+print("Winner: ", winner,"\n-----------------------------", file = pypollwriter)
+
+# The End
