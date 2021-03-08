dc5d89613275e2719cc3290d0ed94b29cc4df653
Dan Cusick <dan.cusick@gmail.com>
2021-03-07 20:54:51 -0600

    handled PyPoll numvotes with rowsum, again commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,3 +7,19 @@
 #
 import os
 import csv
+
+csvpath = os.path.join('.', 'Resources', 'election_data.csv')
+
+with open(csvpath) as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter = ',')
+    csv_header = next(csvreader)
+    print(f"Header: {csv_header}")
+    num_votes = sum(1 for row in csvfile)
+    #print(csvreader.line_num)
+    #print("num_votes", num_votes)
+
+print("\n Election Results \n","-----------------------------")
+#print(" Total Votes:", (csvreader.line_num - 1),"\n-----------------------------")
+print(" Total Votes:", num_votes,"\n-----------------------------")
+#print(" Winner: ", pnl_total,"\n-----------------------------")
