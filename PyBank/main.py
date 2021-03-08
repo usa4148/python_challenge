7eb826565c4f1c46cab5dc9d7970380f1aafbad3
Dan Cusick <dan.cusick@gmail.com>
2021-03-07 18:12:59 -0600

    formatted PyBank main.py commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,6 +8,9 @@
 import os
 import csv
 
+pybank = os.path.join('.', 'PyBank.txt')
+pybankwriter = open(pybank,'w')
+
 csvpath = os.path.join('.', 'Resources', 'budget_data.csv')
 
 with open(csvpath) as csvfile:
@@ -44,5 +47,12 @@ with open(csvpath) as csvfile:
     print(" Total Months:", csvreader.line_num - 1)
     print(" Total: $", pnl_total)
     print(" Average Change: $", "{:.2f}".format(change_sum / (csvreader.line_num - 1)))
-    print(" Greatest Increase in Profits: ", pnl_max_date, pnl_max_value)
-    print(" Greatest Decrease in Profits: ", pnl_min_date, pnl_min_value)
+    print(" Greatest Increase in Profits: ", pnl_max_date, "($",pnl_max_value,")")
+    print(" Greatest Decrease in Profits: ", pnl_min_date, "($",pnl_min_value,")")
+
+    print("\n Financial Analysis \n","-----------------------------", file = pybankwriter)
+    print(" Total Months:", csvreader.line_num - 1, file = pybankwriter)
+    print(" Total: $", pnl_total, file = pybankwriter)
+    print(" Average Change: $", "{:.2f}".format(change_sum / (csvreader.line_num - 1), file = pybankwriter))
+    print(" Greatest Increase in Profits: ", pnl_max_date, "($",pnl_max_value,")", file = pybankwriter)
+    print(" Greatest Decrease in Profits: ", pnl_min_date, "($",pnl_min_value,")", file = pybankwriter)
