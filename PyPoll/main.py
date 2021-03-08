23b9b3dcc163b5ae76c0d34986d4b5fab654a56d
Dan Cusick <dan.cusick@gmail.com>
2021-03-07 22:20:15 -0600

    Got the dict working finally commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,6 +7,7 @@
 #
 import os
 import csv
+import numpy
 
 csvpath = os.path.join('.', 'Resources', 'election_data.csv')
 
@@ -14,12 +15,25 @@ with open(csvpath) as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter = ',')
     csv_header = next(csvreader)
-    print(f"Header: {csv_header}")
-    num_votes = sum(1 for row in csvfile)
+    #print(f"Header: {csv_header}")
+
+    #num_votes = sum(1 for row in csvfile)
     #print(csvreader.line_num)
     #print("num_votes", num_votes)
 
+    candidates = {}
+
+    for vote in csvreader:
+        if  vote[2] in candidates:                #candidates.get(vote[2]) != None:
+            candidates[vote[2]] += 1
+        else:
+            candidates[vote[2]] = 1
+            print(f"{vote[2]}")
+
+
+
 print("\n Election Results \n","-----------------------------")
-#print(" Total Votes:", (csvreader.line_num - 1),"\n-----------------------------")
-print(" Total Votes:", num_votes,"\n-----------------------------")
+print(" Total Votes:", (csvreader.line_num - 1),"\n-----------------------------")
+#print(" Total Votes:", num_votes,"\n-----------------------------")
 #print(" Winner: ", pnl_total,"\n-----------------------------")
+print(f"{candidates}")
